b0ff2e68bfad42ffbff90392e1949dee97d4c792
Ken Whitener <hkw@tc3.edu>
2020-02-03 08:37:47 -0500

    add week 3 directory

--- week-2/code/ValueOfDemo.java
+++ week-2/code/ValueOfDemo.java
@@ -29,21 +29,30 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */ 
 
-class MultiDimArrayDemo {
-                                                                                
-    public static void main(String[] args){
+public class ValueOfDemo {
+    public static void main(String[] args) {
 
-        String[][] the_matrix = new String[10][10];
+        // this program requires two 
+        // arguments on the command line 
+        if (args.length == 2) {
+            // convert strings to numbers
+            float a = (Float.valueOf(args[0])).floatValue(); 
+            float b = (Float.valueOf(args[1])).floatValue();
 
-        String[][] names = {
-            {"Mr. ","Mrs. ","Ms. "},
-            {"Smith","Jones"}};
-
-        // Mr. Smith
-        System.out.println(names[0][0] +
-                           names[1][0]);
-        // Ms. Jones
-        System.out.println(names[0][2] + 
-                           names[1][1]);
+            // do some arithmetic
+            System.out.println("a + b = " +
+                               (a + b));
+            System.out.println("a - b = " +
+                               (a - b));
+            System.out.println("a * b = " +
+                               (a * b));
+            System.out.println("a / b = " +
+                               (a / b));
+            System.out.println("a % b = " +
+                               (a % b));
+        } else {
+            System.out.println("This program " +
+                "requires two command-line arguments.");
+        }
     }
 }
