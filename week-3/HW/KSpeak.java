2dfc74a3dd5b8a4bde14144eb14531d6e07f80f6
Patrick Temple <patrick.a.temple2000@gmail.com>
2020-02-09 22:09:53 -0500

    Revised KSpeak.java to remove switch statements

--- week-3/HW/KSpeak.java
+++ week-3/HW/KSpeak.java
@@ -1,7 +1,10 @@
 // Patrick Temple
 // Prof. Whitener
 // CSCI165
-// 7 February 2020
+// 10 February 2020 - HEAVILY REVISED
+
+// Revision reason: needed to eliminate switch statements
+// which were not allowed
 
 // Week 2 HW: KSpeak
 // Purpose: to transliterate numbers
@@ -13,6 +16,35 @@ import java.lang.String;        // for special string methods
 import java.lang.StringBuilder; // for concatenating strings
 
 public class KSpeak {
+
+    public static String translate(String original) { // string translation engine
+        // language diagram
+        String codes = "*BEA@FK%RM";   // current translation pattern, ex. 1 = B
+
+        // variables used in translation
+        String translated = "";        // holds result at the end
+        char currentNumAsChar = ' ';   // temporarily holds current number but as ASCII value
+        int currentNumber = 0;         // holds the actual number to be translated
+        char currentTranslation = '*'; // holds the current KSpeak value aligned with CurrentNumber
+
+        // what constructs my strings
+        StringBuilder makeTranslation = new StringBuilder();
+
+        // make the translation
+        for(int i = 0; i < (original.length()); i++) {
+            currentNumAsChar = original.charAt(i);                       // get current index's number from 
+                                                                         // string to be translated
+            currentNumber = Character.getNumericValue(currentNumAsChar); // store the above result as an actual
+                                                                         // number, not an ASCII value
+            currentTranslation = codes.charAt(currentNumber);            // get the correct translation by matching
+                                                                         // the number with the correct translation
+            makeTranslation.append(currentTranslation);                  // append onto string builder
+        } // end for
+
+        translated = makeTranslation.toString(); // unload the string builder into a string
+        return translated;                       // finally return the value, and return to main
+    } // end function translate
+
     public static void main(String[] args){
 
         // Scanner for input
@@ -23,100 +55,11 @@ public class KSpeak {
         System.out.print("\nEnter a line of numbers to be translated: ");
         String toTranslate = reader.nextLine();
         
-        // get length of inputted numbers
-        int lengthOfInput = toTranslate.length();
-        
-        // see if there are letters or characters with a loop
-        // ( adapted from https://stackoverflow.com/a/18591352 )
-        for(char c : toTranslate.toCharArray()) { // for all characters in inputted numbers
+        // get solution
+        String solution = translate(toTranslate);                // goes to translate function above
+        System.out.printf("Solution should be: %s%n", solution); // print result
 
-            if(!(Character.isDigit(c))) { // character/letter found
-                System.out.println("ERROR: No letters or characters allowed in input.");
-                System.out.println("This program will be terminated.");
-                System.exit(1); // terminate program immediately
-            } // end if
-
-        } // end for
-        
-        // translate the numbers by running the numbers
-        // through a switch statement
-        StringBuilder makeResult = new StringBuilder(); // handles concatenation
-        String translated = ""; // this is where the result lands later
-        for(char c : toTranslate.toCharArray()) {
-            switch (c) {
-
-                // for each matching character, add on
-                // the appropriate translated character
-                // to translate into KenSpeak
-                case '0': {
-                    makeResult.append('*'); // add a * into the StringBuilder
-                    break; // skip the rest of the statements
-                } // end case 0
-
-                // each case works the same as the
-                // first case
-                case '1': {
-                    makeResult.append('B');
-                    break;
-                } // end case 1
-
-                case '2': {
-                    makeResult.append('E');
-                    break;
-                } // end case 2
-
-                case '3': {
-                    makeResult.append('A');
-                    break;
-                } // end case 3
-
-                case '4': {
-                    makeResult.append('@');
-                    break;
-                } // end case 4
-
-                case '5': {
-                    makeResult.append('F');
-                    break;
-                } // end case 5
-
-                case '6': {
-                    makeResult.append('K');
-                    break;
-                } // end case 6
-
-                case '7': {
-                    makeResult.append('%');
-                    break;
-                } // end case 7
-
-                case '8': {
-                    makeResult.append('R');
-                    break;
-                } // end case 8
-
-                case '9': {
-                    makeResult.append('M');
-                    break;
-                } // end case 9
-
-                // if there was a problem that was not
-                // handled prior to this, print a message
-                // to the console
-                default: {
-                    System.out.println("Something is wrong with a statement, and this text should not be showing.");
-                    break;
-                } // end default case
-
-            } // end switch
-
-        } // end for statement
-
-        // give the concatenated string to the user
-        translated = makeResult.toString();
-        System.out.printf("Translated number: %s%n", translated);
-
-        reader.close();
+        reader.close(); // close input reader
 
 
     } // end main
